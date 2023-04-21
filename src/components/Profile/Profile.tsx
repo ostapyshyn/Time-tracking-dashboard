eb85fecb58113ca13dfa523842cf59ef5cb5c752
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-04-21 18:45:27 +0300

    Add profile card

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from './Profile.module.scss'
+const Profile = () => {
+  return (
+    <section>
+      <img src="" alt="" />
+      <h2>Report for</h2>
+      <h1>Jeremy Robson</h1>
+      <ul>
+        <li>Daily</li>
+        <li>Weekly</li>
+        <li>Monthly</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Profile
